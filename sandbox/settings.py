c44407d691b95105940b49b3b71419b359384eac
Martijn Jacobs <maerteijn@gmail.com>
2021-04-28 20:19:58 +0200

    Added wagtail-generic-chooser dependency and app

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "generic_chooser",
     "wagtail_active_campaign",
     "testapp",
 ]
