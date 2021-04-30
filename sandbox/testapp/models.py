a429305c251d51993119a443f23cea6d8a7b9b89
Martijn Jacobs <maerteijn@gmail.com>
2021-04-30 09:55:23 +0200

    Cleanup the wagtail_active_campaign models to be abstract only
    And move the implementation to the used page models

--- sandbox/testapp/models.py
+++ sandbox/testapp/models.py
@@ -1,24 +1,55 @@
-from wagtail.admin.edit_handlers import FieldPanel, ObjectList, TabbedInterface
+from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    InlinePanel,
+    ObjectList,
+    TabbedInterface,
+)
 from wagtail.core.fields import RichTextField
 
-from wagtail_active_campaign.models import ActiveCampaignForm
+from wagtail_active_campaign.abstract_models import (
+    AbstractActiveCampaignForm,
+    AbstractActiveCampaignFormField,
+)
 
 
-class FormPage(ActiveCampaignForm):
+class FormPage(AbstractActiveCampaignForm):
+    FORM_FIELD = "form_page"
+    FORM_FIELDS_REVERSE = "form_page_fields"
+
     intro = RichTextField(blank=True)
     thank_you_text = RichTextField(blank=True)
 
-    content_panels = ActiveCampaignForm.content_panels + [
+    content_panels = AbstractActiveCampaignForm.content_panels + [
         FieldPanel("intro", classname="full"),
+        InlinePanel(FORM_FIELDS_REVERSE, label="form fields"),
         FieldPanel("thank_you_text", classname="full"),
     ]
-    parent_page_types = ["wagtailcore.Page"]
+
     edit_handler = TabbedInterface(
         [
             ObjectList(content_panels, heading="Form"),
-            ObjectList(ActiveCampaignForm.submission_panels, heading="Submissions"),
-            ObjectList(ActiveCampaignForm.promote_panels, heading="Promote"),
-            ObjectList(ActiveCampaignForm.settings_panels, heading="Settings"),
+            ObjectList(
+                AbstractActiveCampaignForm.submission_panels, heading="Submissions"
+            ),
+            ObjectList(AbstractActiveCampaignForm.promote_panels, heading="Promote"),
+            ObjectList(AbstractActiveCampaignForm.settings_panels, heading="Settings"),
         ]
     )
+
     template = "wagtail_active_campaign/form_page.html"
+
+
+class FormPageFormField(AbstractActiveCampaignFormField):
+    form_page = ParentalKey(
+        "FormPage", on_delete=models.CASCADE, related_name="form_page_fields"
+    )
+
+    class Meta(AbstractActiveCampaignFormField.Meta):
+        constraints = (
+            models.UniqueConstraint(
+                name="A mapping field can only be used once for each form",
+                fields=(FormPage.FORM_FIELD, "mapping"),
+            ),
+        )
--- src/wagtail_active_campaign/models.py
+++ src/wagtail_active_campaign/models.py
@@ -1,17 +1,9 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.db import models
-from modelcluster.fields import ParentalKey
-from wagtail.admin.edit_handlers import (
-    FieldPanel,
-    InlinePanel,
-    MultiFieldPanel,
-    ObjectList,
-    TabbedInterface,
-)
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 
-from .abstract_models import AbstractActiveCampaignForm, AbstractActiveCampaignFormField
 from .client import Client
 
 
@@ -61,45 +53,3 @@ class ActiveCampaignSettings(BaseSetting):
 
     def get_client(self):
         return Client(self.api_url, self.api_key)
-
-
-class ActiveCampaignFormField(AbstractActiveCampaignFormField):
-    form = ParentalKey(
-        "ActiveCampaignForm", on_delete=models.CASCADE, related_name="form_fields"
-    )
-
-    class Meta(AbstractActiveCampaignFormField.Meta):
-        constraints = (
-            models.UniqueConstraint(
-                name="A mapping field can only be used once for each form",
-                fields=("form", "mapping"),
-            ),
-        )
-
-
-class ActiveCampaignForm(AbstractActiveCampaignForm):
-    content_panels = AbstractActiveCampaignForm.content_panels + [
-        InlinePanel("form_fields", label="Form fields"),
-    ]
-
-    edit_handler = TabbedInterface(
-        [
-            ObjectList(content_panels, heading="Form"),
-            ObjectList(
-                AbstractActiveCampaignForm.submission_panels, heading="Submissions"
-            ),
-            ObjectList(AbstractActiveCampaignForm.promote_panels, heading="Promote"),
-            ObjectList(AbstractActiveCampaignForm.settings_panels, heading="Settings"),
-        ]
-    )
-
-    def prepare_data_for_active_campain(self, data):
-        # only use the fields which have a filled out mapping field
-        qs = self.form_fields.exclude(mapping__exact="")  # pylint: disable=no-member
-        qs = qs.values_list("mapping", "clean_name")
-
-        return {mapping: data[clean_name] for mapping, clean_name in qs}
-
-    # Do not make these pages available in the wagtail admin per default
-    # TODO: Make this a setting
-    parent_page_types = []
