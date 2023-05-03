8c0525c34a9a649e9c33abfb8b8cf802fef9e1e0
Martijn Jacobs <maerteijn@gmail.com>
2023-05-03 22:17:03 +0200

    Prevent unneeded migrations in the sandbox

--- sandbox/settings/base.py
+++ sandbox/settings/base.py
@@ -156,7 +156,7 @@ STATIC_URL = "/static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 
 # Wagtail settings
